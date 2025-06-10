ca77d4f40e2ed0d297ccada7c431457efaf0c0d6
sumitmina <sumitmina2004@gmail.com>
2025-06-10 15:59:07 +0530

    fix generate token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (userId, res) => {
     res.cookie("jwt",token,{
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict", // CSRF attacks cross-site equest forgetory attacks
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // CSRF attacks cross-site equest forgetory attacks
         secure: process.env.NODE_ENV != 'development'
     })
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const backend_url = import.meta.env.VITE_BACKEND_URL
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    // baseURL: "http://localhost:8000/api",
+    baseURL: `${backend_url}/api`,
     withCredentials: true
 });
